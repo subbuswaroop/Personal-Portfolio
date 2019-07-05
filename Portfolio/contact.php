753e916aa5eb7f348b7cd87b6df156b4bcbe380e
SubbuSwaroop <subbuswaroop3795@gmail.com>
2019-07-05 12:18:49 +0530

    Final Version Changes

--- Portfolio/contact.php
+++ Portfolio/contact.php
@@ -34,11 +34,8 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']))
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"
-        async></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:700|Josefin+Sans" rel="stylesheet" />
 </head>
